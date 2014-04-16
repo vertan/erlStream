e328f211529743c1d5e7bd97a840bc68c7b9fa37
Filip Hedman <hedman.filip@gmail.com>
2014-04-16 15:28:39 +0200

    Added some initial documentation

--- src/server.erl
+++ src/server.erl
@@ -1,3 +1,13 @@
+%%%         File  : server.erl
+%%%      Authors  : Jeanette Castillo <jeanette.cas@hotmail.com>, Filip Hedman <hedman.filip@gmail.com>,
+%%%                 Robert Kallgren <robertkallgren@gmail.com>, Oscar Mangard <oscarmangard@gmail.com>,
+%%%                 Mikael Sernheim <mikael.sernheim@gmail.com>
+%%%  Description  : The content server handling all requests for the erlStream service.
+%%%      Created  : 16 Apr 2014
+%%%      Version  : 0.01
+
+-vsn(0.01).
+
 -module(server).
 -export([start/0]).
 
