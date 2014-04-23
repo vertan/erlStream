1985b88ceb1c58c757e3765ad4c528f450493104
kallgren <robertkallgren@hotmail.com>
2014-04-23 19:41:13 +0200

    Added user input for sending messages in client

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,21 +1,39 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
+
+    private static boolean running = true;
 
     public static void main(String[] args) throws Exception {
 	System.out.println("Welcome to the client!");
 
-	Socket clientSocket = new Socket("localhost", 8181);
+	Socket clientSocket = new Socket("localhost", 1337);
 	DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
 	BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	String testString = "Hi my sir!";
-	//toServer.writeChars(testString + '\n');
-	toServer.writeBytes("Test!");
-	String answer = "Test"; //fromServer.readLine();
 
-	System.out.println("Answer from server: " + answer);
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Type your commands:");
+
+	while(running) {
+	    System.out.print("> ");
+	    String message = sc.nextLine();
+
+	    switch(message) {
+	    case "quit":
+		running = false;
+		break;
+	    default:
+		toServer.writeBytes(message);
+	    }
+
+	    // String answer = fromServer.readLine();
+	    // System.out.println("Answer from server: " + answer);
+	}
+	
 	clientSocket.close();
+	System.out.println("Goodbye!");
     }
 
 }
