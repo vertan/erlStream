93d510a0c5d1955978ba884d5a757961e7f69dce
kallgren <robertkallgren@hotmail.com>
2014-04-23 21:40:37 +0200

    Added support for sending server address and port through arguments when launching Client

--- client/src/Client.java
+++ client/src/Client.java
@@ -5,17 +5,30 @@ import java.util.Scanner;
 /**
  * @author Jeanette Castillo <jeanette.cas@hotmail.com>, Filip Hedman <hedman.filip@gmail.com>, Robert Kallgren <robertkallgren@gmail.com>, Oscar Mangard <oscarmangard@gmail.com>, Mikael Sernheim <mikael.sernheim@gmail.com>
  *
- * The client class provides methods to communicate with a server. 
+ * The client class provides methods to communicate with a server.
  *
+ * @param address The address of the server
+ * @param port The port of the server
  */
 public class Client {
 
     private static boolean running = true;
 
     public static void main(String[] args) throws Exception {
+	String address = "localhost";
+	int port = 1337;
+
+	if (args.length > 0) {
+	    address = args[0];
+	    
+	    if (args.length > 1) {
+		port = Integer.parseInt(args[1]);
+	    }
+	}
+
 	System.out.println("Welcome to the client!");
 
-	Socket clientSocket = new Socket("localhost", 1337);
+	Socket clientSocket = new Socket(address, port);
 	DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
 	BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
