84d39d845b01759f62a46a9b1c168d73aed3ad1c
kallgren <robertkallgren@hotmail.com>
2014-05-06 19:34:14 +0200

    Updated documentation and added GUI option in Client.java

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,13 +1,19 @@
 /**
  * @author Jeanette Castillo <jeanette.cas@hotmail.com>, Filip Hedman <hedman.filip@gmail.com>, Robert Kallgren <robertkallgren@gmail.com>, Oscar Mangard <oscarmangard@gmail.com>, Mikael Sernheim <mikael.sernheim@gmail.com>
  *
- * The client class provides methods to communicate with a server.
+ * The Client class launches a music player in either a graphical or a command line interface.
+ *
+ * @see CLI
+ * @see GUI
  */
 public class Client {
 
     public static void main(String[] args) {
-	CLI cli = new CLI();
-	cli.start();
+	if (args.length > 0 && args[0].equals("gui")) {
+	    //new GUI().start();
+	} else {
+	    new CLI().start();
+	}
     }
 
 }
