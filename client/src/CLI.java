f5eb69b8a8c09e765feb4df836350c79d1ba4645
kallgren <robertkallgren@hotmail.com>
2014-05-06 16:13:24 +0200

    Added Communicator class (Main API) And CLI class

---
+++ client/src/CLI.java
@@ -0,0 +1,82 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+/**
+ * @author Jeanette Castillo <jeanette.cas@hotmail.com>, Filip Hedman <hedman.filip@gmail.com>, Robert Kallgren <robertkallgren@gmail.com>, Oscar Mangard <oscarmangard@gmail.com>, Mikael Sernheim <mikael.sernheim@gmail.com>
+ *
+ * This class provides a Command Line Interface for the Communicator module.
+ *
+ * @see Communicator
+ */
+public class CLI {
+    private Communicator accomodator;
+    private Scanner sc;
+    private boolean running;
+    private String answer;
+
+    public CLI() {
+	String address = "localhost";
+	String input;
+
+	sc = new Scanner(System.in);
+	System.out.print("Server address (Press ENTER for localhost): ");
+	input = sc.nextLine();
+
+	if (!input.equals("")) {
+	    address = input;
+	}	    
+
+	accomodator = new Communicator(address, 1341, 1340);
+    }
+
+    public void start() {
+	// TODO: Verify connection before printing anything
+
+	System.out.println("Welcome to the client!");
+	System.out.println("Type your commands:");
+
+	running = true;
+
+	while(running) {
+	    System.out.print("> ");
+	    String input = sc.nextLine();
+
+	    String[] command = input.split(" ");
+	    
+	    switch(command[0]) {
+	    case "quit":
+		running = false;
+		break;
+	    case "list":
+		try {
+		    List<Song> songs = accomodator.list();		    
+		    for (int i = 0; i < songs.size(); i++) {
+			System.out.println(songs.get(i).getFileName());
+		    }
+		}
+		catch (Throwable e) {
+		    System.out.println("Error " + e.getMessage());
+		    e.printStackTrace();
+		}
+
+		break;
+	    case "play":
+		try {
+		    accomodator.play(new Song(command[1], "Unknown Title", "Unknown Artist", "Unknown Album", 60), 0);
+		}
+		catch (Throwable e) {
+		    System.out.println("Error " + e.getMessage());
+		    e.printStackTrace();
+		}
+
+		break;
+	    default:
+		System.out.println("Unknown command");
+	    }
+	}
+	
+	System.out.println("Goodbye!");
+    }
+}
---
+++ client/src/Communicator.java
@@ -0,0 +1,89 @@
+import java.util.List;
+import java.util.ArrayList;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+import java.io.*;
+import java.net.*;
+
+/**
+ * @author Jeanette Castillo <jeanette.cas@hotmail.com>, Filip Hedman <hedman.filip@gmail.com>, Robert Kallgren <robertkallgren@gmail.com>, Oscar Mangard <oscarmangard@gmail.com>, Mikael Sernheim <mikael.sernheim@gmail.com>
+ *
+ * This class communicates with a server
+ */
+public class Communicator {
+    private String address;
+    private int inPort;
+    private int outPort;
+
+    private Socket inSocket, outSocket;
+    private DataOutputStream toServer;
+    private BufferedReader fromServer;
+    
+    public Communicator(String address, int inPort, int outPort) {
+	this.address = address;
+	this.inPort = inPort;
+	this.outPort = outPort;
+    }
+
+    private String getBaseName(String filename) {
+	String[] splitname = filename.split("\\.(?=[^\\.]+$)");
+	return splitname[0];
+    }
+    
+    /**
+     * Returns a list containing the the available songs on the server
+     *
+     * @return The songs available on the server
+     * @see Song
+     */
+    public List<Song> list() throws Exception {
+	inSocket = new Socket(address, inPort);
+	outSocket = new Socket(address, outPort);
+	toServer = new DataOutputStream(outSocket.getOutputStream());
+	fromServer = new BufferedReader(new InputStreamReader(inSocket.getInputStream()));
+	
+	toServer.writeBytes("list");
+	outSocket.close();
+	
+	List<Song> songs = new ArrayList<Song>();
+
+	String answer = fromServer.readLine();
+	
+	while(answer != null) {
+	    songs.add(new Song(getBaseName(answer), "Unknown Title", "Unknown Artist", "Unknown Album", 60));
+	    answer = fromServer.readLine();
+	}
+
+	return songs;
+    }
+
+    /**
+     * Plays the song song if it exists on the server.
+     *
+     * @param song The song to play 
+     * @param time The number of seconds to skip
+     * @return true if the file existed on the server and the playback succeeded, else false
+     */
+    public boolean play(Song song, int time) throws Exception {
+	inSocket = new Socket(address, inPort);
+	outSocket = new Socket(address, outPort);
+	toServer = new DataOutputStream(outSocket.getOutputStream());
+	fromServer = new BufferedReader(new InputStreamReader(inSocket.getInputStream()));
+    
+	toServer.writeBytes("play " + song.getFileName() + ".mp3");
+	outSocket.close();
+    
+	InputStream audioData = inSocket.getInputStream();
+	AdvancedPlayer musicPlayer = new AdvancedPlayer(audioData);
+	musicPlayer.play();
+
+	// TODO: Check whether playback succeeded
+	return true;
+    }
+
+    /**
+     * Pauses the current playing song.
+     */
+    public void pause() {
+	// TODO: Comm
+    }
+}
