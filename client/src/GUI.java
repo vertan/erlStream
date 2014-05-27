9b99c7d78e1941aa24b995ea46ddf2a46182e04e
Filip Hedman <hedman.filip@gmail.com>
2014-05-27 17:16:12 +0200

    Functioning images in jar

--- client/src/GUI.java
+++ client/src/GUI.java
@@ -173,8 +173,7 @@ public void launchGUI() {
     Thread update = new Thread(new Update());
     update.start();
 
-    playButton = new JButton (new ImageIcon("play.png"));
-        //playButton = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("play.png")));
+    playButton = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("play.png")));
     playButton.setBorder(null);
     playButton.setOpaque(false);
     playButton.setContentAreaFilled(false);
@@ -183,24 +182,20 @@ public void launchGUI() {
         @Override
         public void mousePressed(MouseEvent e){
             if(playButt){
-                playButton.setIcon(new ImageIcon("pressedPlay.png"));
-                    //playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedPlay.png")));
+		playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedPlay.png")));
             } else {
-                playButton.setIcon(new ImageIcon("pressedPause.png"));
-                    //playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedPause.png")));
+		playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedPause.png")));
             }
         }
         @Override
         public void mouseClicked(MouseEvent e){
             try{
                 if(playButt){
-                    playButton.setIcon(new ImageIcon("pause.png"));
-                        //playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pause.png")));
+		    playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pause.png")));
                     playButt = false;
                     manager.play();
                 } else {
-                    playButton.setIcon(new ImageIcon("play.png"));
-                            //playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("play.png")));	
+		    playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("play.png")));	
                     playButt = true;
                     manager.pause();
                 }
@@ -213,8 +208,7 @@ timeline = new JSlider(JSlider.HORIZONTAL,0,100,0);
 timeline.setPreferredSize(new Dimension(450,20));
 timeline.addChangeListener(new SlideListener());
 
-nextButton = new JButton (new ImageIcon("next.png"));
-        //nextButton = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("next.png")));
+nextButton = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("next.png")));
 nextButton.setBorder(null);
 nextButton.setOpaque(false);
 nextButton.setContentAreaFilled(false);
@@ -222,14 +216,12 @@ nextButton.setBorderPainted(false);
 nextButton.addMouseListener(new MouseAdapter(){
     @Override
     public void mousePressed(MouseEvent e){
-        nextButton.setIcon(new ImageIcon("pressedNext.png"));    
-                //nextButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedNext.png")));
+	nextButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedNext.png")));
     }
     @Override
     public void mouseClicked(MouseEvent e){
         try{
-            nextButton.setIcon(new ImageIcon("next.png")); 
-                    //nextButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("next.png")));
+	    nextButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("next.png")));
             manager.next();
         } catch(Exception b) {System.out.println("next failed");}
     }
@@ -239,8 +231,7 @@ nextButton.addMouseListener(new MouseAdapter(){
 curentTime = new JLabel("0:00");
 songTime = new JLabel("0:00");
 
-previousButton = new JButton(new ImageIcon("previous.png"));
-        //previousButton = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("previous.png")));
+previousButton = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("previous.png")));
 previousButton.setBorder(null);
 previousButton.setOpaque(false);
 previousButton.setContentAreaFilled(false);
@@ -248,22 +239,19 @@ previousButton.setBorderPainted(false);
 previousButton.addMouseListener(new MouseAdapter(){
     @Override
     public void mousePressed(MouseEvent e){
-        previousButton.setIcon(new ImageIcon("pressedPrevious.png"));
-                //previousButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedPrevious.png")));
+	previousButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedPrevious.png")));
     }
     @Override
     public void mouseClicked(MouseEvent e){
         try{
-            previousButton.setIcon(new ImageIcon("previous.png"));
-                    //previousButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("previous.png")));
+	    previousButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("previous.png")));
             manager.previous();
         } catch(Exception b) {System.out.println("next failed");}
     }
 });
 
         //toggleShuffle
-toggleShuffle = new JButton(new ImageIcon("shuffleSvart.png"));
-        //toggleShuffle = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("shuffleLila.png")));
+toggleShuffle = new JButton (new ImageIcon(this.getClass().getClassLoader().getResource("shuffleSvart.png")));
 toggleShuffle.setBorder(null);
 toggleShuffle.setOpaque(false);
 toggleShuffle.setContentAreaFilled(false);
@@ -273,13 +261,11 @@ toggleShuffle.addMouseListener(new MouseAdapter(){
     public void mouseClicked(MouseEvent e){
         try{
             if(shuffelBool){
-                toggleShuffle.setIcon(new ImageIcon("shuffleLila.png"));
-                        //toggleShuffle.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("shuffleSvart.png")));
+		toggleShuffle.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("shuffleLila.png")));
                 manager.setShuffle(!manager.shuffleIsOn());
                 shuffelBool = false;
             } else {
-                toggleShuffle.setIcon(new ImageIcon("shuffleSvart.png"));
-                        //toggleShuffle.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("shuffleLila.png")));
+		toggleShuffle.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("shuffleSvart.png")));
                 manager.setShuffle(!manager.shuffleIsOn());
                 shuffelBool = true;
             }
@@ -288,7 +274,7 @@ toggleShuffle.addMouseListener(new MouseAdapter(){
 });
 
         //toggleRepeat
-toggleRepeat = new JButton(new ImageIcon("pressedRepeat.png"));
+toggleRepeat = new JButton((new ImageIcon(this.getClass().getClassLoader().getResource("pressedRepeat.png"))));
 toggleRepeat.setBorder(null);
 toggleRepeat.setOpaque(false);
 toggleRepeat.setContentAreaFilled(false);
@@ -298,13 +284,11 @@ toggleRepeat.addMouseListener(new MouseAdapter(){
     public void mouseClicked(MouseEvent e){
         try{
             if(repeatBool){
-                toggleRepeat.setIcon(new ImageIcon("repeatBlack.png"));
-                        //toggleRepeat.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedRepeat.png")));
+		toggleRepeat.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("repeatBlack.png")));
                 manager.setRepeat(!manager.repeatIsOn());
                 repeatBool = false;
             } else {
-                toggleRepeat.setIcon(new ImageIcon("pressedRepeat.png"));
-                        //toggleRepeat.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("repeat.png")));
+		toggleRepeat.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pressedRepeat.png")));
                 manager.setRepeat(!manager.repeatIsOn());
                 repeatBool = true;
             }
@@ -365,8 +349,7 @@ songTable.addMouseListener(new MouseAdapter() {
         try {
             manager.playSongByTitle(title.toString(),0);
             playButt = false;
-            playButton.setIcon(new ImageIcon("pause.png"));
-                        //playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pause.png")));
+	    playButton.setIcon(new ImageIcon(this.getClass().getClassLoader().getResource("pause.png")));
             target.clearSelection();
         } catch (Exception ex) {System.out.println("List Click failed!");}
     }
