8b9b7640d8aef872ad056da2d2d19daa070c426f
kallgren <robertkallgren@hotmail.com>
2014-06-05 23:08:38 +0200

    Updated documentation of BadSongException and PlaybackFailedException

--- client/src/BadSongException.java
+++ client/src/BadSongException.java
@@ -1,9 +1,21 @@
 /**
- * Signals that an AudioManager was given a song that does not exist in it's library.
+ * Thrown to indicate that an <code>AudioManager</code> was given a song that does not exist in its library.
  *
  * @see AudioManager
  */
 public class BadSongException extends Exception {
+    /**
+     * Constructs a new <code>BadSongException</code> with no detail message.
+     */
+    public  BadSongException() {
+	super();
+    }
+    
+    /**
+     * Constructs a new <code>BadSongException</code> with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public BadSongException(String message) {
 	super(message);
     }
--- client/src/PlaybackFailedException.java
+++ client/src/PlaybackFailedException.java
@@ -1,9 +1,21 @@
 /**
- * Signals that an error occured when trying to play audio.
+ * Thrown to indicate that an <code>AudioManager</code> encountered an error when trying to play a song.
  *
  * @see AudioManager
  */
 public class PlaybackFailedException extends Exception {
+    /**
+     * Constructs a new <code>PlaybackFailedException</code> with no detail message.
+     */
+    public PlaybackFailedException() {
+	super();
+    }
+
+    /**
+     * Constructs a new <code>PlaybackFailedException</code> with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public PlaybackFailedException(String message) {
 	super(message);
     }
