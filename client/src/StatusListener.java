0221aeacb6d60ee68bb2384674acc929a84a0ba5
kallgren <robertkallgren@hotmail.com>
2014-06-05 22:41:12 +0200

    Updated documentation of StatusListener.java

--- client/src/StatusListener.java
+++ client/src/StatusListener.java
@@ -1,13 +1,32 @@
 import java.util.List;
 
 /**
- * This interface is used to receive status updates from the AudioManager class.
+ * This interface is used to receive status updates from the <code>AudioManager</code> class.
  *
  * @see AudioManager
  */
 public interface StatusListener {
+    /**
+     * Invoked if the <code>AudioManager</code> receives an updated song list from the server.
+     *
+     * @param newSongs the songs available on the server
+     */
     public void songsUpdated(List<Song> newSongs);
+
+    /**
+     * Invoked if the server is shut down.
+     */
     public void serverShutdown();
+
+    /**
+     * Invoked if the <code>AudioManager</code> loses its connection to the server.
+     */
     public void connectionLost();
+
+    /**
+     * Invoked if the <code>AudioManager</code> regains connection to the server after being disconnected.
+     *
+     * @param songs the songs available on the server
+     */
     public void connectionRegained(List<Song> songs);
 }
